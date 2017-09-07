1492100bc7f4cec187f3a7b5362148bb723d2086
ednilson <ednilson_queiroz@hotmail.com>
2017-09-07 16:58:04 -0400

    Menu

---
+++ Camera/app/src/main/java/com/example/ednilson/camera/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ednilson.camera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public  boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.action_hello:
+                Log.d("MainActivity -> ","Hello Menu");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
